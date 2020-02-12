53d58078e921ce772fc747862393f400da7bdb68
omriyaacobi <55637769+omriyaacobi@users.noreply.github.com>
2020-02-12 17:29:46 +0200

    Add files via upload

--- omri.py
+++ omri.py
@@ -1,18 +1,20 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu Dec 12 14:03:09 2019
+Created on Sun Nov  3 11:49:37 2019
 
 @author: Student
 """
-# example of calculation 1d convolutions from numpy
-import numpy as np 
-#from numpy import asarray 
-a=np.array([[-5,2,3,3],[1,2,4,5],[1,2,1,4]])
-a[0][0]=-5
-b=a[0:1,0:2]
 
-print (b)
-'''
-print (a.shape)
-print (a)
-'''
+
+
+import numpy as np
+
+def ArrayMul(a,kernel):
+    b=np.zeros((len(a)-2))
+    for x in range(len(b)):        
+        b[x]=np.dot((np.array([a[x],a[x+1],a[x+2]])),kernel)
+    return b
+
+a=np.array([0,1,0,0,1,0,0,0])
+kernel=np.array([0,1,0])
+print(ArrayMul(a,kernel))
